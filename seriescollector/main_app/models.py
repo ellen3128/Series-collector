2c13e28c98b520f576e4ff5d2ae75c18b9c4f5e4
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-21 14:26:37 -0400

    adds models with class Series

--- seriescollector/main_app/models.py
+++ seriescollector/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Series(models.Model):
+    title = models.CharField(max_length=100)
+    seasons = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    rating = models.IntegerField()
