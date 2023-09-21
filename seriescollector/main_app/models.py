2ab204a0c00a8087b2fcf8cb0afcb1d4c309b36d
ellen3128 <hyunpark.ellen@gmail.com>
2023-09-21 14:41:20 -0400

    adds series data using Shell

--- seriescollector/main_app/models.py
+++ seriescollector/main_app/models.py
@@ -6,3 +6,11 @@ class Series(models.Model):
     seasons = models.IntegerField()
     genre = models.CharField(max_length=100)
     rating = models.IntegerField()
+
+#   {'title': 'You', 'seasons': 4, 'genre': 'Thriller', 'rating': 7.7 }, 
+#   {'title': 'Money Heist', 'seasons': 5, 'genre': 'Thriller', 'rating': 8.2 }, 
+#   {'title': 'Black Mirror', 'seasons': 6, 'genre': 'Sci-fi', 'rating': 8.7 }, 
+#   {'title': 'Inventing Anna', 'seasons': 1, 'genre': 'Drama', 'rating': 6.8 }, 
+
+def __str__(self):
+    return self.name
